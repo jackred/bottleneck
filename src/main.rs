cf8641bda57d7b894e3cc923586ece90dcbb6d67
jackred <jackred@tuta.io>
2021-11-08 23:54:39 +0000

    ADD log message info
    doing things
    not sure what
    but doing them

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use teloxide::prelude::*;
-use teloxide::types;
+use teloxide::types::{MessageKind, MediaKind, User};
 
 extern crate chrono;
 use chrono::{DateTime, TimeZone, NaiveDateTime, Utc};
@@ -12,6 +12,33 @@ use std::io::Read;
 async fn main() {
     run().await;
 }
+
+fn unwrap_or_default<T, U>(to_unwrap: Option<T>, default: U, unwrap_fn: &dyn Fn(T)-> U) -> U {
+    match to_unwrap {
+	Some(res) => unwrap_fn(res),
+	None => default
+    }
+}
+
+fn unwrap_user(user: User) -> String{
+    let mut res = String::from(user.first_name);
+    if let Some(name) = user.last_name {
+	res += " ";
+	res += &name;
+    }
+    if let Some(name) = user.username {
+	res += " (";
+	res += &name;
+	res += ")";
+    }
+    res
+}
+
+
+fn unwrap_sender(user: Option<User>) -> String{
+    unwrap_or_default(user, "channel message".to_string(), &unwrap_user)
+}
+
 
 async fn run() -> Result<()>{
     let mut file = File::open("./config.json").unwrap();
@@ -27,27 +54,97 @@ async fn run() -> Result<()>{
     teloxide::repl(bot, |message| async move {
         //message.answer_dice().await?;
 	let msg = message.update;
-	let res = match msg.kind {
-	    types::MessageKind::Common(cm) => match cm.media_kind {
-		types::MediaKind::Text(mkt) => mkt.text,
-		_ => String::new()
-	    }
-	    _ => String::new()
+	let mut res = String::new();
+	match msg.kind {
+	    MessageKind::Common(mk) => {
+		res += "Common Message";
+		res = format!("{} from {}", res, unwrap_sender(mk.from));
+		match mk.media_kind {
+		    MediaKind::Text(mkt) => {
+			res = format!("{}{}{}", res, "\n", mkt.text);
+		    },
+		    _ => {}
+		}},
+	    MessageKind::NewChatMembers(mk) => {
+		res += "Member new to the chat";
+	    },
+	    MessageKind::LeftChatMember(mk) => {
+		res += "Member left the chat";
+	    },
+	    MessageKind::NewChatTitle(mk) => {
+		res += "New chat title";
+	    },
+	    MessageKind::NewChatPhoto(mk) => {
+		res += "New chat photo";
+	    },
+	    MessageKind::DeleteChatPhoto(mk) => {
+		res += "Chat photo deleted";
+	    },
+	    MessageKind::GroupChatCreated(mk) => {
+		res += "New group chat";
+	    },
+	    MessageKind::SupergroupChatCreated(mk) => {
+		res += "New super group chat";
+	    },
+	    MessageKind::ChannelChatCreated(mk) => {
+		res += "New channel created";
+	    },
+	    MessageKind::MessageAutoDeleteTimerChanged(mk) => {
+		res += "Timer delete message changed";
+	    },
+	    MessageKind::Migrate(mk) => {
+		res += "Migrated?";
+	    },
+	    MessageKind::Pinned(mk) => {
+		res += "Pinned";
+	    },
+	    MessageKind::Invoice(mk) => {
+		res += "Invoice?";
+	    },
+	    MessageKind::SuccessfulPayment(mk) => {
+		res += "Succesful payment?";
+	    },
+	    MessageKind::ConnectedWebsite(mk) => {
+		res += "Connected website?";
+	    },
+	    MessageKind::PassportData(mk) => {
+		res += "Passport data?";
+	    },
+	    MessageKind::Dice(mk) => {
+		res += "Dice?";
+	    },
+	    MessageKind::ProximityAlertTriggered(mk) => {
+		res += "Proximity alert triggered?";
+	    },
+	    MessageKind::VoiceChatScheduled(mk) => {
+		res += "Voice chat scheduled";
+	    },
+	    MessageKind::VoiceChatStarted(mk) => {
+		res += "Voice chat started";
+	    },
+	    MessageKind::VoiceChatEnded(mk) => {
+		res += "Voice chat ended";
+	    },
+	    MessageKind::VoiceChatParticipantsInvited(mk) => {
+		res += "Voice chat participant invited";
+	    },
+	    _ => {}
 	};
-	log::info!("{}: {:?}", NaiveDateTime::from_timestamp(msg.date.into(), 0), res);
-	let options = vec![
-	    String::from("Lundi"),
-	    String::from("Mardi"),
-	    String::from("Mercredi"),
-	    String::from("Jeudi"),
-	    String::from("Vendredi"),
-	    String::from("Samedi"),
-	    String::from("Dimanche"),
-	];
-	let poll = message.requester.send_poll(msg.chat.id, "Quel jour?", options, types::PollType::Regular);
-	let poll = poll.allows_multiple_answers(true);
-	let poll = poll.is_anonymous(false);
-	poll.await?;
+	log::info!("{}: {:?}", NaiveDateTime::from_timestamp(msg.date.into(), 0), "Message received!");
+	println!("{}", res);
+	// let options = vec![
+	//     String::from("Lundi"),
+	//     String::from("Mardi"),
+	//     String::from("Mercredi"),
+	//     String::from("Jeudi"),
+	//     String::from("Vendredi"),
+	//     String::from("Samedi"),
+	//     String::from("Dimanche"),
+	// ];
+	// let poll = message.requester.send_poll(msg.chat.id, "Quel jour?", options, types::PollType::Regular);
+	// let poll = poll.allows_multiple_answers(true);
+	// let poll = poll.is_anonymous(false);
+	// poll.await?;
 	log::info!("Replying");
         respond(())
     })
