db5ab02cbf40f5ff34f0bcc190a126b147e75d63
jackred <jackred@tuta.io>
2021-11-13 11:23:42 +0000

    ADD command handling
    use command instead of text

--- src/main.rs
+++ src/main.rs
@@ -1,153 +1,67 @@
-use teloxide::prelude::*;
-use teloxide::types::{MessageKind, MediaKind, User};
+use teloxide::types;
+use teloxide::{prelude::*, utils::command::BotCommand};
 
 extern crate chrono;
-use chrono::{DateTime, TimeZone, NaiveDateTime, Utc};
+use chrono::{NaiveDateTime};
 
-use serde_json::{Result, Value};
+use serde_json;
 use std::fs::File;
 use std::io::Read;
+use std::error::Error;
 
 #[tokio::main]
 async fn main() {
     run().await;
 }
 
-fn unwrap_or_default<T, U>(to_unwrap: Option<T>, default: U, unwrap_fn: &dyn Fn(T)-> U) -> U {
-    match to_unwrap {
-	Some(res) => unwrap_fn(res),
-	None => default
-    }
+#[derive(BotCommand)]
+#[command(rename = "lowercase", description = "These commands are supported:")]
+enum Command {
+    #[command(description = "display this text.")]
+    Help,
+    #[command(description = "send a poll with all days of the week")]
+    Poll
 }
 
-fn unwrap_user(user: User) -> String{
-    let mut res = String::from(user.first_name);
-    if let Some(name) = user.last_name {
-	res += " ";
-	res += &name;
-    }
-    if let Some(name) = user.username {
-	res += " (";
-	res += &name;
-	res += ")";
-    }
-    res
+async fn answer(
+    cx: UpdateWithCx<AutoSend<Bot>, Message>,
+    command: Command,
+) -> Result<(), Box<dyn Error + Send + Sync>> {
+    match command {
+        Command::Help => {cx.answer(Command::descriptions()).await?;}
+	Command::Poll => {
+	    let options = vec![
+    		String::from("Lundi"),
+    		String::from("Mardi"),
+    		String::from("Mercredi"),
+    		String::from("Jeudi"),
+    		String::from("Vendredi"),
+    		String::from("Samedi"),
+    		String::from("Dimanche"),
+    	    ];
+	    let poll = cx.requester.send_poll(cx.update.chat.id, "Quel jour?", options, types::PollType::Regular);
+	    let poll = poll.allows_multiple_answers(true);
+	    let poll = poll.is_anonymous(false);
+	    poll.await?;
+	}
+	
+    };
+
+    Ok(())
 }
 
-
-fn unwrap_sender(user: Option<User>) -> String{
-    unwrap_or_default(user, "channel message".to_string(), &unwrap_user)
-}
-
-
-async fn run() -> Result<()>{
+async fn run() -> serde_json::Result<()>{
     let mut file = File::open("./config.json").unwrap();
     let mut data = String::new();
     file.read_to_string(&mut data).unwrap();
     
-    let v: Value = serde_json::from_str(&data)?;
+    let v: serde_json::Value = serde_json::from_str(&data)?;
     
     teloxide::enable_logging!();
 
     let bot = Bot::new(v["token"].as_str().unwrap()).auto_send();
 
-    teloxide::repl(bot, |message| async move {
-        //message.answer_dice().await?;
-	let msg = message.update;
-	let mut res = String::new();
-	match msg.kind {
-	    MessageKind::Common(mk) => {
-		res += "Common Message";
-		res = format!("{} from {}", res, unwrap_sender(mk.from));
-		match mk.media_kind {
-		    MediaKind::Text(mkt) => {
-			res = format!("{}{}{}", res, "\n", mkt.text);
-		    },
-		    _ => {}
-		}},
-	    MessageKind::NewChatMembers(mk) => {
-		res += "Member new to the chat";
-	    },
-	    MessageKind::LeftChatMember(mk) => {
-		res += "Member left the chat";
-	    },
-	    MessageKind::NewChatTitle(mk) => {
-		res += "New chat title";
-	    },
-	    MessageKind::NewChatPhoto(mk) => {
-		res += "New chat photo";
-	    },
-	    MessageKind::DeleteChatPhoto(mk) => {
-		res += "Chat photo deleted";
-	    },
-	    MessageKind::GroupChatCreated(mk) => {
-		res += "New group chat";
-	    },
-	    MessageKind::SupergroupChatCreated(mk) => {
-		res += "New super group chat";
-	    },
-	    MessageKind::ChannelChatCreated(mk) => {
-		res += "New channel created";
-	    },
-	    MessageKind::MessageAutoDeleteTimerChanged(mk) => {
-		res += "Timer delete message changed";
-	    },
-	    MessageKind::Migrate(mk) => {
-		res += "Migrated?";
-	    },
-	    MessageKind::Pinned(mk) => {
-		res += "Pinned";
-	    },
-	    MessageKind::Invoice(mk) => {
-		res += "Invoice?";
-	    },
-	    MessageKind::SuccessfulPayment(mk) => {
-		res += "Succesful payment?";
-	    },
-	    MessageKind::ConnectedWebsite(mk) => {
-		res += "Connected website?";
-	    },
-	    MessageKind::PassportData(mk) => {
-		res += "Passport data?";
-	    },
-	    MessageKind::Dice(mk) => {
-		res += "Dice?";
-	    },
-	    MessageKind::ProximityAlertTriggered(mk) => {
-		res += "Proximity alert triggered?";
-	    },
-	    MessageKind::VoiceChatScheduled(mk) => {
-		res += "Voice chat scheduled";
-	    },
-	    MessageKind::VoiceChatStarted(mk) => {
-		res += "Voice chat started";
-	    },
-	    MessageKind::VoiceChatEnded(mk) => {
-		res += "Voice chat ended";
-	    },
-	    MessageKind::VoiceChatParticipantsInvited(mk) => {
-		res += "Voice chat participant invited";
-	    },
-	    _ => {}
-	};
-	log::info!("{}: {:?}", NaiveDateTime::from_timestamp(msg.date.into(), 0), "Message received!");
-	println!("{}", res);
-	// let options = vec![
-	//     String::from("Lundi"),
-	//     String::from("Mardi"),
-	//     String::from("Mercredi"),
-	//     String::from("Jeudi"),
-	//     String::from("Vendredi"),
-	//     String::from("Samedi"),
-	//     String::from("Dimanche"),
-	// ];
-	// let poll = message.requester.send_poll(msg.chat.id, "Quel jour?", options, types::PollType::Regular);
-	// let poll = poll.allows_multiple_answers(true);
-	// let poll = poll.is_anonymous(false);
-	// poll.await?;
-	log::info!("Replying");
-        respond(())
-    })
-	.await;
+    let bot_name: String = "bot_name".to_string();
+    teloxide::commands_repl(bot, bot_name, answer).await;
     Ok(())
 }
